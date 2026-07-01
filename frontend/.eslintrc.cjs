61d7f92189ec8c058e416aced0873b03251ae327
pranavgawasproject <pranavgawas.project@gmail.com>
2026-07-01 21:03:33 +0000

    chore(frontend): adjust eslint rules to permit warnings, unused variables, and disable react/prop-types checking

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -13,9 +13,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
   },
 }
